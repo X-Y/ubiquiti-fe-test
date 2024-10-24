cdc2c17e4cfbfa82a77b099be2a162754b26df5d
Xun Yang <xun.yang@1337.tech>
2024-10-24 20:57:20 +0200

    Fix logo background

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,7 +9,7 @@ const Header = ({ title, user }: HeaderProps) => {
     <div className="w-full bg-neutral-03 h-[50px] flex flex-row items-center text-text-3 font-light sticky top-0 z-10">
       <IconButton
         icon="logo"
-        className="bg-transparent rounded-none h-full w-[50px] [&_img]:w-auto [&_img]:h-auto"
+        className="!bg-transparent rounded-none h-full w-[50px] [&_img]:w-auto [&_img]:h-auto"
       />
       <h1 className="text-sm ml-3">{title}</h1>
       <div className="ml-auto mr-8 text-sm">{user}</div>
